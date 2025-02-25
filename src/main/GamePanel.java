1381d368a7e6c67c550bf0d4a2bc5dfc00d31ce3
h263073 <h263073@stud.u-szeged.hu>
2025-02-25 11:09:33 +0100

    making img appear

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,19 +3,34 @@ package main;
 import Inputs.KeyboardInputs;
 import Inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private float xPos =150, yPos =150;
     private float xSpeed=10, ySpeed=10;
+    private BufferedImage img;
     public GamePanel(){
         setPanelSize();
+        importImg();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
+    void importImg(){
+        InputStream is = getClass().getResourceAsStream("/sprites/test_Player_sprite.png");
+        try {
+            img= ImageIO.read(is);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
     }
     private void setPanelSize() {
         Dimension size = new Dimension(1280,680);
@@ -36,5 +51,6 @@ public class GamePanel extends JPanel {
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        g.drawImage(img,0,0,128,128,null);
     }
 }
