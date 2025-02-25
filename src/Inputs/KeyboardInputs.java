fa6932b57fd58b3dd8c490fce250ed40b1259985
h263073 <h263073@stud.u-szeged.hu>
2025-02-25 20:53:17 +0100

    now able to swich between animations by moving, able to turn, no time between button pressed and actual action. still a bug: when changing direction have to wait trought 1 animation loop

--- src/Inputs/KeyboardInputs.java
+++ src/Inputs/KeyboardInputs.java
@@ -4,7 +4,7 @@ import main.GamePanel;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+import static utilz.Constants.Directions.*;
 public class KeyboardInputs implements KeyListener {
     private GamePanel gamePanel;
     public KeyboardInputs(GamePanel gp){
@@ -18,39 +18,31 @@ public class KeyboardInputs implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode()==KeyEvent.VK_W){
-            gamePanel.setYPos(-gamePanel.getYSpeed());
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_W:
+                gamePanel.setDirection(UP);
+                break;
+            case KeyEvent.VK_D:
+                gamePanel.setDirection(RIGHT);
+                break;
+            case KeyEvent.VK_S:
+                gamePanel.setDirection(DOWN);
+                break;
+            case KeyEvent.VK_A:
+                gamePanel.setDirection(LEFT);
+                break;
         }
-        if(e.getKeyCode()==KeyEvent.VK_A){
-            gamePanel.setXPos(-gamePanel.getXSpeed());
-        }
-        if(e.getKeyCode()==KeyEvent.VK_S){
-            gamePanel.setYPos(gamePanel.getYSpeed());
-        }
-        if(e.getKeyCode()==KeyEvent.VK_D){
-            gamePanel.setXPos(gamePanel.getXSpeed());
-        }
-/*
-        if(e.getKeyCode()==KeyEvent.VK_W && e.getKeyCode()==KeyEvent.VK_A){
-            gamePanel.setYPos(-gamePanel.getYSpeed()*1.414213f);
-            gamePanel.setXPos(-gamePanel.getXSpeed()*1.414213f);
-        }
-        if(e.getKeyCode()==KeyEvent.VK_W && e.getKeyCode()==KeyEvent.VK_D){
-            gamePanel.setYPos(-gamePanel.getYSpeed()*1.414213f);
-            gamePanel.setXPos(gamePanel.getXSpeed()*1.414213f);
-        }
-        if(e.getKeyCode()==KeyEvent.VK_Y && e.getKeyCode()==KeyEvent.VK_A){
-            gamePanel.setYPos(gamePanel.getYSpeed()*1.414213f);
-            gamePanel.setXPos(-gamePanel.getXSpeed()*1.414213f);
-        }
-        if(e.getKeyCode()==KeyEvent.VK_Y && e.getKeyCode()==KeyEvent.VK_D){
-            gamePanel.setYPos(gamePanel.getYSpeed()*1.414213f);
-            gamePanel.setXPos(gamePanel.getXSpeed()*1.414213f);
-        }*/
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_D:
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_A:
+                gamePanel.isMoving(false);
+                break;
+        }
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -9,15 +9,22 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import static utilz.Constants.PlayerConstants.*;
+import static utilz.Constants.Directions.*;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private float xPos =0, yPos =0;
     private float xSpeed=10, ySpeed=10;
     private BufferedImage img;
-    private BufferedImage[] idleAnim;
+    private BufferedImage[][] animations;
     private int animTimer=0; //incrases by frame;
-    private int animIndex, animSpeed=120/6; //means 120/(120/6) means 6 frames per secound
+    private int animIndex, animSpeed=120/6; //means 120/(120/x) means x frames per second
+    private int playerAction=RUN;
+    private int playerDirection=-1;
+    private boolean moving=false;
+    private int playerDrawDir=1; //1 if facing right -1 if facing left
+    private int playerDrawDirPositionCorrection=0; //correct the position while mirroring
     public GamePanel(){
         setPanelSize();
         importImg();
@@ -29,9 +36,11 @@ public class GamePanel extends JPanel {
     }
 
     private void loadAnimation() {
-        idleAnim=new BufferedImage[8];
-        for (int i=0; i< idleAnim.length;i++){
-            idleAnim[i]=img.getSubimage(i*16,0*16,16,16);
+        animations=new BufferedImage[4][8];
+        for (int i=0; i< animations.length;i++){
+            for (int j=0; j<animations[i].length;j++){
+                animations[i][j]=img.getSubimage(j*16,i*16,16,16);
+            }
         }
     }
 
@@ -48,36 +57,63 @@ public class GamePanel extends JPanel {
         Dimension size = new Dimension(1280,680);
         setPreferredSize(size);
     }
-    public float getXSpeed(){
-        return xSpeed;
+    public void setDirection(int direction){
+        this.playerDirection=direction;
+        moving=true;
     }
-    public float getYSpeed(){
-        return ySpeed;
-    }
-    public void setXPos(float value){
-        xPos +=value;
-    }
-    public void setYPos(float value){
-        yPos +=value;
+    public void isMoving(boolean moving){
+        this.moving=moving;
     }
     public void updateGame(){
-        updateAnim();
+        updateAnimLoop();
+        setAnimation();
+        updatePos();
     }
 
-    private void updateAnim() {
+    private void updateAnimLoop() {
         animTimer++;
 
         if(animTimer>=animSpeed){
             animTimer=0;
             animIndex++;
-            if(animIndex>=idleAnim.length){
+            if(animIndex>=getSpritesAmount(playerAction)){
                 animIndex=0;
+            }
+        }
+    }
+    private void setAnimation(){
+        if(moving){
+            playerAction=RUN;
+        }
+        else {
+            playerAction=IDLE;
+        }
+    }
+    private void updatePos(){
+        if(moving){
+            switch (playerDirection){
+                case UP:
+                    yPos-=1;
+                    break;
+                case DOWN:
+                    yPos+=1;
+                    break;
+                case RIGHT:
+                    xPos+=1;
+                    playerDrawDir=1;
+                    playerDrawDirPositionCorrection=0;
+                    break;
+                case LEFT:
+                    xPos-=1;
+                    playerDrawDir=-1;
+                    playerDrawDirPositionCorrection=128;
+                    break;
             }
         }
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawImage(idleAnim[animIndex],(int)xPos,(int)yPos,128,128,null);
+        g.drawImage(animations[playerAction][animIndex],(int)xPos+playerDrawDirPositionCorrection,(int)yPos,128*playerDrawDir,128,null);
     }
 }
