dd9adeee1afd2406e7c9e0800534b0d7957691ec
h263073 <h263073@stud.u-szeged.hu>
2025-03-09 18:49:46 +0100

    making map appear

--- src/levels/Level.java
+++ src/levels/Level.java
@@ -1,4 +1,11 @@
 package levels;
 
 public class Level {
+    private int[][] map;
+    public Level(int[][] map){
+        this.map=map;
+    }
+    public int[][] getMap(){
+        return map;
+    }
 }
--- src/levels/LevelManager.java
+++ src/levels/LevelManager.java
@@ -10,12 +10,25 @@ import utilz.LoadSave;
 public class LevelManager {
     private GamePanel gamePanel;
     private BufferedImage platformSprite[];
+    private Level testLevel;
+    private int[][] testMap= {
+            {9,9,9,9,9,9,9,9,9,9},
+            {9,9,9,9,9,9,9,9,9,9},
+            {9,9,9,9,9,9,2,0,0,0},
+            {9,9,9,9,9,9,9,9,9,9},
+            {0,0,0,0,0,0,0,0,0,0},
+            {1,1,1,1,1,1,1,1,1,1}
+    };
+
     public LevelManager(GamePanel game){
         this.gamePanel =game;
         //platformSprite=LoadSave.getSprite(LoadSave.PLATFORM_SPRITE);
         loadOutSideImg();
+        loadLevels();
     }
-
+    private void loadLevels(){
+        testLevel=new Level(testMap);
+    }
     private void loadOutSideImg() {
         BufferedImage img =LoadSave.getSprite(LoadSave.PLATFORM_SPRITE);
         platformSprite=new BufferedImage[9];
@@ -33,7 +46,14 @@ public class LevelManager {
     }
 
     public void draw(Graphics g){
-        g.drawImage(platformSprite[8],0,0, GameControl.TILE_SIZE,GameControl.TILE_SIZE,null);
+        //g.drawImage(platformSprite[8],0,0, GameControl.TILE_SIZE,GameControl.TILE_SIZE,null);
+        for (int i=0;i< testLevel.getMap().length;i++){
+            for(int j=0; j<testLevel.getMap()[i].length;j++){
+                if(testLevel.getMap()[i][j]<9){
+                    g.drawImage(platformSprite[testLevel.getMap()[i][j]],j*GameControl.TILE_SIZE,i*GameControl.TILE_SIZE, GameControl.TILE_SIZE,GameControl.TILE_SIZE,null);
+                }
+            }
+        }
     }
     public void update(){}
 }
